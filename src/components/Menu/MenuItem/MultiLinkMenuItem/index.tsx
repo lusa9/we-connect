7f2fda01efcd77fe03c98ec11f2e5d875623367c
Luka Ušalj <luka.usalj@gmail.com>
2022-02-15 23:07:00 +0100

    cleaned up unused code

--- src/components/Menu/MenuItem/MultiLinkMenuItem/index.tsx
+++ src/components/Menu/MenuItem/MultiLinkMenuItem/index.tsx
@@ -1,10 +1,7 @@
 import { useMediaMinWidths } from "hooks/usemedia";
-import { useState } from "react";
 import { MenuItemProps } from "types/MenuItemProps";
-import MenuItem from "..";
 import MultiLinkMenuItemDesktop from "./MultiLinkMenuItemDesktop";
 import MultiLinkMenuItemMobile from "./MultiLinkMenuItemMobile";
-import styles from "./styles.module.css";
 
 export interface MultiLinkMenuItemProps {
   slug: string;
