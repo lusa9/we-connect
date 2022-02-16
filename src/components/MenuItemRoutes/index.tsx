52b5f341a5ea2ac43f3c1c52bc166d7483b1d2df
Luka Ušalj <luka.usalj@gmail.com>
2022-02-16 12:03:23 +0100

    removed relative container

--- src/components/MenuItemRoutes/index.tsx
+++ src/components/MenuItemRoutes/index.tsx
@@ -7,23 +7,21 @@ export default () => {
   const { menuItems } = useContext(MenuItemContext);
 
   return (
-    <div className={styles.relativeContainer}>
-      <Routes>
-        {menuItems
-          ?.flatMap(({ children, ...props }) => (children ? children : [props]))
-          .map(({ slug, title }) => (
-            <Route
-              key={slug}
-              path={slug}
-              element={
-                <div className={styles.component}>
-                  <h1>{title}</h1>
-                </div>
-              }
-            />
-          ))}
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </div>
+    <Routes>
+      {menuItems
+        ?.flatMap(({ children, ...props }) => (children ? children : [props]))
+        .map(({ slug, title }) => (
+          <Route
+            key={slug}
+            path={slug}
+            element={
+              <div className={styles.component}>
+                <h1>{title}</h1>
+              </div>
+            }
+          />
+        ))}
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
   );
 };
